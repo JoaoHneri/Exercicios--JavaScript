9793ebf4fc6351f825afc4bdf580e2730042466a
João Henrique <joaohnsousa@gmail.com>
2023-02-04 17:35:06 -0300

    Exercícios
    Feitos acompanhando O Dicover Rocketseat

---
+++ scripts.js
@@ -0,0 +1,111 @@
+/*
+
+Exercícios tirado da rocketseat  Discover:
+
+
+
+
+Esse exercício vai trabalhar a transformação de notas escolares, onde devemos criar um algoritmo para transformar notas númericas em notas de caracteres.
+
+Desafio:
+
+Crie um algoritmo que transforme as notas do sistema
+numérico para sistema de notas em caracteres tipo A B C D F
+
+* de 90 para cima -   A
+* entre 80 - 89   -   B
+ * entre 70 - 79   -   C
+ * entre 60 - 69   -   D
+ * menor que 60    -   F
+
+
+
+
+ function avaliar(nota){
+     if(nota >= 90){
+     console.log("Parabéns Nota A");
+     }else if(nota >=80 && nota <= 89){
+     console.log("Parabéns Nota B");
+     }else if(nota >=70 && nota <= 79){
+     console.log("Nota C, você pode melhorar!");
+     }else if(nota >=60 && nota <= 69){
+     console.log("Nota D, melhorar ainda é possivel!");
+     }else{
+     console.log("Nota F, uma pena");
+ }
+ }
+
+avaliar(60);
+ */
+
+//##################################################################################
+
+/*Crie um objeto que possuirá 2 propriedades, ambas do tipo array:
+    * receitas: [] 
+    * despesas: []
+Agora, crie uma função que irá calcular o total de receitas e 
+despesas e irá mostrar uma mensagem se a família está com 
+saldo positivo ou negativo, seguido do valor do saldo.
+
+
+let family = {
+    ganhos: [1000],
+    gastos: [800]
+}
+
+function somarArray(array){
+    total = 0;
+    for(let value of array){
+        total +=value;
+    }
+    return total
+}
+
+function calculate(){
+    const somaGanhos = somarArray(family.ganhos)
+    const somarGastos = somarArray(family.gastos)
+
+
+    const total = somaGanhos - somarGastos
+    
+    const ItsOkay = total >=0
+    let mensage = "negativo"
+
+    if(ItsOkay){
+        mensage = "positivo"
+    }
+
+    console.log(`Seu saldo é ${mensage}: ${total}`)
+}
+
+calculate()
+
+*/
+
+function transformar(graus){
+    const celsius = graus.toUpperCase().includes('C')
+    const fahrenheint = graus.toUpperCase().includes('F')
+
+    if(!celsius && !fahrenheint){
+        throw new Error('Grau não identificado')
+    }
+
+    let novoGrau = Number(graus.toUpperCase().replace("F", ""));
+    let formula = (fahrenheint) => (fahrenheint - 32) * 5/9
+    let novoSimbol = 'C'
+
+    if(celsius){
+         novoGrau = Number(graus.toUpperCase().replace("C", ""));
+         formula = celsius => celsius * 9/5 + 32
+         novoSimbol = 'F'
+    }
+
+    return formula(novoGrau) + novoSimbol
+}
+try {
+    console.log(transformar('10C'))
+    console.log(transformar('50F'))
+    console.log(transformar('50Z'))
+}catch(error){
+    console.log(error.message)
+}
